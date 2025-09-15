31d686e13dc548d5bdf9dd9f385d4255da89d7e3
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-15 21:32:03 +0530

    update edit user page

--- frontend/src/pages/EditUser.jsx
+++ frontend/src/pages/EditUser.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import API from "../api";
+import UserForm from "../components/UserForm";
 
-const EditUser = () => {
+export default function EditUser() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    API.get(`/users/${id}`).then(res => setUser(res.data));
+  }, [id]);
+
+  const handleSubmit = async (data) => {
+    await API.put(`/users/${id}`, data);
+    navigate("/");
+  };
+
   return (
-    <div>
-      edit user
+    <div className="p-6">
+      <h2 className="text-xl font-bold mb-4">Edit User</h2>
+      {user && <UserForm onSubmit={handleSubmit} initialData={user} />}
     </div>
-  )
+  );
 }
-
-export default EditUser
