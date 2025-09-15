72179780bf1f8d6c45a1789ac5b166ed6a1ff193
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-15 19:40:37 +0530

    create user model

---
+++ backend/models/User.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const User = sequelize.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  company: {
+    type: DataTypes.STRING,
+  },
+  street: {
+    type: DataTypes.STRING,
+  },
+  city: {
+    type: DataTypes.STRING,
+  },
+  zipcode: {
+    type: DataTypes.STRING,
+  },
+  lat: {
+    type: DataTypes.STRING,
+  },
+  lng: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = User;
